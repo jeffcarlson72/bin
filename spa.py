122ddf1c448e97a52d6c03fc4a3aa366f0bdda9c
Jeff Carlson <jeff@ultimateevil.org>
2021-07-03 14:28:10 -0700

    hmac.new and socket.send require bytes instead of strings now

--- spa.py
+++ spa.py
@@ -11,11 +11,11 @@ if len(sys.argv) != 5:
     print(f"Usage:  {sys.argv[0]} <secret> <src-ip> <dst-ip> <knock-port>")
     sys.exit(1)
 
-m = hmac.new(sys.argv[1], digestmod=SHA256)
+m = hmac.new(bytes(sys.argv[1], 'utf-8'), digestmod=SHA256)
 m.update(inet_aton(gethostbyname(sys.argv[2])))
 m.update(struct.pack("i", (int)(time.time() / 60)))
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.connect((sys.argv[3], (int)(sys.argv[4])))
-s.send(m.hexdigest() + "\n")
+s.send(bytes(m.hexdigest() + "\n", 'utf-8'))
 s.close()
